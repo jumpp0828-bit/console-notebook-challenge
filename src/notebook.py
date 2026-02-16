a77a2dbbf00fcc993b667d66f2669a9875648380
Santi-a-ux <sgutierrez767@soyudemedellin.edu.co>
2026-02-16 17:27:27 -0500

    Creacion de la clase note y definicion de algunos parametros

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,20 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str,):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
